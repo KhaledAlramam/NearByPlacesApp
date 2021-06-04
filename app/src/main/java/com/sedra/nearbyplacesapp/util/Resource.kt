901718b56bd212f9984af4ae1f178aef3456fcc3
KhaledAlramam <khaledalramam@gmail.com>
2021-06-05 00:27:32 +0200

    - Add code comments

--- app/src/main/java/com/sedra/nearbyplacesapp/util/Resource.kt
+++ app/src/main/java/com/sedra/nearbyplacesapp/util/Resource.kt
@@ -1,5 +1,9 @@
 package com.sedra.nearbyplacesapp.util
 
+/**
+ * Class to represent the 3 states of network call
+ * each will be emitted to live data and handled according to logic
+ */
 sealed class Resource<T> {
     object Loading : Resource<Nothing>()
     data class Success<T>(val data: T) : Resource<T>()
